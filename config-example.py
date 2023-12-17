985a7eafdce8dfe824427c35a88a2e8a53958e49
mojyack <mojyack@gmail.com>
2023-12-17 11:35:19 +0900

    mv config.py to config-example.py

--- config-example.py
+++ config-example.py
@@ -3,7 +3,11 @@ load_threshold = 1000 # g
 loop_interval = 0.1
 camera_device = "/dev/video0"
 photo_format = "width=1920,height=1080"
-photo_count = 5
+photo_count = 4
 reset_timeout = 300 # sec
 tmpdir = "/tmp"
 debug = True
+
+# remote
+bot_token="BOT_TOKEN"
+channel_id=CHANNEL_ID
